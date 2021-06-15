973de10ff434afe0b89c3b71da71484df05ad0a9
Dhruvil <51009478+dhruvilpatel07@users.noreply.github.com>
2021-06-15 03:45:29 -0400

    Setup storyboard
    - Create Navigation Controller
    - Added Animation to Launch Screen

--- ChatApp/Controllers/WelcomeViewController.swift
+++ ChatApp/Controllers/WelcomeViewController.swift
@@ -14,7 +14,16 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = ""
+        var charIndex = 0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { [self] (timer) in
+                titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+            
+        }
     }
     
 
