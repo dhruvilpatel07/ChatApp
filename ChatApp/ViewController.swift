c97321a8b85b6a761462df50d5ef7830e46f827c
Dhruvil <51009478+dhruvilpatel07@users.noreply.github.com>
2021-06-15 03:06:57 -0400

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Dhruvil Patel on 2021-06-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
